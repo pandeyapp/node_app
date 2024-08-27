4347f586cd9be29d122bd3bbcc662ebbbca8ceff
pandeyapp <pandeysakshi2211@gmail.com>
2024-08-27 16:49:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 // Configure the HTTP server to respond with "Hello World" to all requests.
 const hostname = '0.0.0.0';
-const port = 3001;
+const port = 2020;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
